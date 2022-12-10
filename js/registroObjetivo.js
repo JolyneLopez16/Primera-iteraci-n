49aebd949bc554d8409d82c3e3a99807971e6703
juanca876 <113404771+juanca876@users.noreply.github.com>
2022-12-10 01:28:38 -0600

    validacion registro objetivo

---
+++ js/registroObjetivo.js
@@ -0,0 +1,35 @@
+const inCondicion = document.getElementById('inp-condicion');
+const inTipoObjetivo = document.getElementById('slt-tipo-objetivo');
+const btnGuardarObjetivo = document.getElementById('btn-guardar-objetivo');
+
+const validar = async() => {
+    let error = false;
+    let camposRequeridos = document.querySelectorAll('.requerido');
+
+    camposRequeridos.forEach(campo => {
+        if (campo.value == '') {
+            campo.classList.add('input-error');
+            error = true;
+        } else {
+            campo.classList.remove('input-error')
+        }
+    });
+
+    if (error) {
+        Swal.fire({
+            'icon': 'warning',
+            'title': '¡No se ha registrado el cambio!',
+            'text': 'Por favor llene todos los espacios.'
+        });
+    } else {
+        Swal.fire({
+                'icon': 'success',
+                'title': '¡Has ingresado un nuevo objetivo correctamente!',
+                'text': 'Podés revisarlo en tu perfil.'
+
+            })
+            /*registrarObjetivoNuevo()*/
+    }
+}
+
+btnGuardarObjetivo.addEventListener('click', validar)
